20b62fe66a98f11db8cd5a6545d3300b15a66ffe
Saptarshi Mandal <saptarshicoding@gmail.com>
2023-03-09 19:57:49 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_forward_crypto_app/homepage.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Crypto Wallet',
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
